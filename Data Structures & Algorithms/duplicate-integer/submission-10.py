9a6b774e1e1063db0bd8ce5675867201576b88da
Srivats Pulumati <146019365+Srivats242@users.noreply.github.com>
2026-09-17 07:18:14 -0400

    Add: duplicate-integer - submission-10

---
+++ Data Structures & Algorithms/duplicate-integer/submission-10.py
@@ -0,0 +1,35 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+      #Hashset correct solution
+      hashset = set()
+
+      for n in nums:
+        if n in hashset:
+            return True
+        hashset.add(n)
+      return False
+    
+
+
+
+      #Numbers hashmap personal solution
+      #nums = {}
+      #nums[0] = 1
+      #nums[1] = 2
+      #nums[2] = 3
+      
+      #for i in range len(nums_hashmap_length):
+      #for key, value in nums.items():
+        #if nums_hashmap[i] == nums_hashmap[i+1]:
+            #return True
+        #print(f"Key {key}, Value {value}")
+        #print(f"Key {key + 1}, Value {value + 1}")
+        #print(f"Key {key + 2}, Value {value + 2}")
+        #print(f"Key {key + 3}, Value {value +2}")
+        #if (nums[key] == nums[key + 1]):
+            #return True
+       #else: 
+            #return False
+
+
+      
